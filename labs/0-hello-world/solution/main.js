03d85773bcbfac789b9a23446f34496b826244cc
xXanth0s <maxi.schore@gmail.com>
2021-03-23 21:12:48 +0100

    add package-lock.json

--- labs/0-hello-world/solution/main.js
+++ labs/0-hello-world/solution/main.js
@@ -14,21 +14,3 @@ function createWindow() {
 
 // calls function when app is started
 app.whenReady().then(createWindow);
-
-// closes electron process, when window is closed
-app.on('window-all-closed', () => {
-  // on MacOS, app is minimized to taskbar, when all windows are closed
-  // User must close app with "CMD + Q"
-  // process.platform is 'darwin' in MacOS
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-// executes, when app is activated. Occurs in multiple different scenarios
-app.on('activate', () => {
-  // when no window is open, main window will be created. Mostly on MacOS
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-})
